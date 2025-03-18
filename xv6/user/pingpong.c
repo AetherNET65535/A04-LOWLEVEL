997e053d62cd812ed2bab56d17e83834be0677f5
AetherNET65535 <aethernet65535@gmail.com>
2025-03-19 05:40:00 +0800

    Auto push: 2025-03-19 05:40:00

--- xv6/user/pingpong.c
+++ xv6/user/pingpong.c
@@ -2,5 +2,30 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main ()
+int main (int argc, char *argv[])
 {
+    //CHECK: is user enter argument samw with 1?
+    if (argc != 1)
+    {
+        fprinf(2, "USAGE: pingpong\n");
+        exit(1);
+    }
+
+    char my_pipe[2];
+
+    if (fork() == 0) // CHILD
+    {
+        char child_signal[1]; 
+        read(my_pipe[0], child_signal, 1); // READ: yes, just read
+        printf("%d: received ping\n", getpid());
+        write(my_pipe[1], "C", 1); // SEND: i am ur son
+    }
+    else
+    {
+        char parent_signal[1];
+        write(my_pipe[0], "P", 1); // SEND: i am ur dad
+        read(my_pipe[1], parent_signal, 1); // READ: just read
+        printf("%d: received pong\n", getpid());
+    }
+    
+
