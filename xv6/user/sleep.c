e43bda81f728eb888a62a9c5d6ed21331f7d269d
AetherNET65535 <aethernet65535@gmail.com>
2025-03-18 13:00:01 +0800

    Auto push: 2025-03-18 13:00:01

--- xv6/user/sleep.c
+++ xv6/user/sleep.c
@@ -2,8 +2,31 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main (int argc, int argv)
+int main (int argc, char *argv[])
 {
-    int i;
+    // CHECK: is user enter argument same with 2?
+    if (argc != 2)
+    {
+        fprintf (2, "USAGE: sleep <ticks>\n");
+        exit(1);
+    }
+    
+    // CHANGE: ASCII to INTERGER
+    int ticks = atoi(argv[1]);
+
+    // CHECK: if ticks smaller than 0, set it to 0
+    if (ticks < 0)
+    {
+        ticks = 0;
+    }
+
+    // CALL: sleep (thank you xv6, this make this code ez 5000%)
+    sleep(ticks);
+
+    exit(0);
+}
+
+
+
     
    
