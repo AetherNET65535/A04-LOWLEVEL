945fda6f1c4a4f84860f6a7cfc756a404fd5baca
AetherNET65535 <aethernet65535@gmail.com>
2025-03-19 13:20:01 +0800

    Auto push: 2025-03-19 13:20:01

--- xv6/user/pingpong.c
+++ xv6/user/pingpong.c
@@ -10,36 +10,6 @@ int main (int argc, char *argv[])
         fprintf(2, "USAGE: pingpong\n");
         exit(1);
     }
-
-    int my_pipe[2];
-    pipe(my_pipe);
-
-    char msg[32];
-    int len;
-
-    if (fork() == 0) // CHILD
-    {
-        char child_signal[1];
-
-        read(my_pipe[1], child_signal, 1); // READ: yes, just read
-        int len = snprintf(msg, "%d: received ping\n", getpid());
-        write(1, msg, len);
-        write(my_pipe[1], "C", 1); // SEND: i am ur son
-
-        close(my_pipe[1]); // CLOSE: son dont need to write
-        close(my_pipe[0]); // CLOSE: son dont need to read rn
-    }
-    else
-    {
-        char parent_signal[1];
-
-        write(my_pipe[1], "P", 1); // SEND: i am ur dad
-        read(my_pipe[0], parent_signal, 1); // READ: just read, dad will wait son
-        int len = snprintf(msg, "%d: received pong\n", getpid());
-        write(1, msg, len);
-
-        close(my_pipe[0]); // CLOSE: dad dont need to read rn        
-        exit(0);
-    }
+    
 }
 
