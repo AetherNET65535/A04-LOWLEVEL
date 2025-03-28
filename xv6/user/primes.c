722b216f7fc45c733ed5a2b5d1fab77179f608a6
AetherNET65535 <aethernet65535@gmail.com>
2025-03-28 08:30:01 +0800

    Auto push: 2025-03-28 08:30:01

--- xv6/user/primes.c
+++ xv6/user/primes.c
@@ -1,63 +1,82 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#define READ  0
-#define WRITE 1
-#define stdin 0
-#define stdout 1
-#define stderr 2
 
-void sieveProc(int left[2]) {
+#define READ 0
+#define WRITE 1
+
+void sieve_algo (int left[2])
+{
     close(left[WRITE]);
 
     int prime, temp, pid, right[2];
 
-    // if doesn't read a single number from left, then exit
-    if( read(left[READ], &prime, sizeof(int)) == 0 ) {
+    // if cant read any number...QAQ
+    if (read(left[READ], &prime, sizeof(int)) == 0)
+    {
         close(left[READ]);
         exit(0);
     }
 
-    printf("prime %d\n", prime);
+    printf("prime: %d\n", prime);
     pipe(right);
-    if( (pid = fork()) < 0 ) {
-        fprintf(stderr, "fork error...\n");
+
+    if ((pid = fork()) > 0)
+    {
         close(right[READ]);
-        close(right[WRITE]);
-        close(left[READ]);
-        exit(1);
-    } else if( pid > 0 ) {
-        close(right[READ]);
-        while(read(left[READ], &temp, sizeof(int))) {
-            if( temp % prime == 0 ) continue;
-            write(right[WRITE], &temp, sizeof(int));
+
+        while(read(left[READ], &temp, sizeof(int)))
+        {
+            if (temp % prime != 0)
+            {
+                write(right[WRITE], &temp, sizeof(int));
+            }
         }
         close(right[WRITE]);
         wait(0);
         exit(0);
-    } else {
-        sieveProc(right);
+    }
+    else if (pid == 0)
+    {
+        sieve_algo(right);
         exit(0);
+    }
+    else
+    {
+        fprintf(2, "fork error...\n");
+        close(right[READ]);
+        close(right[WRITE]);
+        close(left[READ]);
+        exit(1);
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     int pid, p[2];
+    pipe(p); // give 'p' a acess to this p (prob kernel will give him, idk)
 
-    pipe(p);
-    if((pid = fork()) < 0) {
-        fprintf(stderr, "fork error...\n");
-        exit(1);
-    } else if (pid > 0) {
+    if ((pid = fork()) > 0) // parent
+    {
         close(p[READ]);
-        for(int i = 2; i <= 35; ++i) {
+
+        for (int i = 2; i <= 35; i++)
+        {
             write(p[WRITE], &i, sizeof(int));
         }
+
         close(p[WRITE]);
-        wait(0);
+        wait(0); // wait child kill himself
+        exit(0); // kys
+    }
+    else if (pid == 0) // child
+    {
+        sieve_algo(p);
         exit(0);
-    } else {
-        sieveProc(p);
-        exit(0);
+    }
+    else // 5000% have error
+    {
+        fprintf(2, "fork error\n");
+        exit(1);
     }
 }
